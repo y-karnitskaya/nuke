0da3fe897e7f9dcb92cc7b6e0f21d8767f1356e6
Matthias Koch <ithrowexceptions@gmail.com>
2023-05-27 19:34:52 +0200

    Add string.NullIfEmpty extension methods

--- source/Nuke.Utilities/Text/String.Emptiness.cs
+++ source/Nuke.Utilities/Text/String.Emptiness.cs
@@ -19,12 +19,32 @@ public static partial class StringExtensions
     }
 
     /// <summary>
-    /// Indicates whether a specified string is null, empty, or consists only of white-space characters.
+    /// Indicates whether a specified string is null, empty, or consists only of whitespace characters.
     /// </summary>
     [Pure]
     [ContractAnnotation("null => halt")]
     public static bool IsNullOrWhiteSpace(this string str)
     {
         return string.IsNullOrWhiteSpace(str);
+    }
+
+    /// <summary>
+    /// Returns the same string if it's not empty, otherwise null.
+    /// </summary>
+    [Pure]
+    [ContractAnnotation("null => null")]
+    public static string NullIfEmpty(this string str)
+    {
+        return !str.IsNullOrEmpty() ? str : null;
+    }
+
+    /// <summary>
+    /// Returns the same string if it's not only whitespaces, otherwise null.
+    /// </summary>
+    [Pure]
+    [ContractAnnotation("null => null")]
+    public static string NullIfWhiteSpace(this string str)
+    {
+        return !str.IsNullOrWhiteSpace() ? str : null;
     }
 }
